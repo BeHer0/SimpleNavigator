c5c1e3ac5c14acea320361aa57cc6565209eaee2
SimpleLife <2239268289@qq.com>
2023-09-09 09:30:15 +0800

    add: gitee link

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,3 +1,4 @@
+import GiteeLink from '@/components/GiteeLink';
 import Head from '@/components/Head';
 import Logo from '@/components/Logo';
 import {repository} from '@/config';
@@ -19,6 +20,9 @@ export default {
     placeholder: '搜索',
   },
   navigation: false,
+  navbar: {
+    extraContent: <GiteeLink />,
+  },
   // 底部版权标识
   footer: {
     text: (
